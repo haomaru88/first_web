eafbcd5f09ae7944ccb98c2f5b29f842d675513a
haomaru88 <haomaru88@gmail.com>
2020-01-18 16:01:03 +0900

    a

--- application/models/home_model.php
+++ application/models/home_model.php
@@ -39,8 +39,8 @@ class Home_model extends CI_Model
 
       $this->table = 'table_data';
       $this->table_index = 'table_index';
-
       $this->mysqli = new mysqli('localhost', 'juno', 'haomaru98', 'gematek_buoy');
+      
    }
 
    /*
