7d847e5b8d0607993c7a342e11c6a23a618d8639
haomaru88 <haomaru88@gmail.com>
2020-02-03 10:43:38 +0900

    commit on 'master'

--- application/views/home_header.php
+++ application/views/home_header.php
@@ -10,7 +10,7 @@
    <title>Web Monitoring System</title>
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="shortcut icon" type="image/png" href="/htdocs/assets/images/icon/favicon.ico">
-   <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="/assets/css/bootstrap.css">
    <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
    <link rel="stylesheet" href="/assets/css/themify-icons.css">
    <link rel="stylesheet" href="/assets/css/metisMenu.css">
