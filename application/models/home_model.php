2a90adbeb5a8c64960f90983ca44da96c4a0b605
haomaru88 <haomaru88@gmail.com>
2021-01-25 09:10:28 +0900

    Update home_model.php
    Rename RDS tables

--- application/models/home_model.php
+++ application/models/home_model.php
@@ -42,8 +42,11 @@ class Home_model extends CI_Model
       parent::__construct();
 
       $this->load->database();
-      $this->table_data = 'table_data';
-      $this->table_index = 'table_index';
+      $this->table_data = 'tdata_2019';
+      $this->table_index = 'tindex_2019';
+      // $this->table_data = 'table_data';
+      // $this->table_index = 'table_index';
+
       // $this->sql1 = "SELECT * FROM {$this->table_index}";
       // $this->mysqli = new mysqli('localhost', 'juno', 'haomaru98', 'gematek_buoy');
    }
@@ -135,9 +138,9 @@ class Home_model extends CI_Model
       $para['layer'] = $result[0]->layer;
 
       $para += $this->get_latest_data();
-      $prev_one_year = date_create("2019-12-02");
+      $prev_one_year = date_create("2019-11-30");
       // $prev_one_year = date_create();  // 현재 날짜를 얻는다.
-      date_sub($prev_one_year, date_interval_create_from_date_string('1 week'));  // 현재 날짜에서 지정된 기간을 뺀다. 1년치 데이터를 얻는다.
+      date_sub($prev_one_year, date_interval_create_from_date_string('2 weeks'));  // 현재 날짜에서 지정된 기간을 뺀다. 1년치 데이터를 얻는다.
       // date_sub($prev_one_year, date_interval_create_from_date_string('1 year'));  // 현재 날짜에서 지정된 기간을 뺀다. 1년치 데이터를 얻는다.
 
       $target_day = date_format($prev_one_year, 'Y-m-d');   // 지정된 포멧으로 날짜를 변환한다.
@@ -167,28 +170,3 @@ class Home_model extends CI_Model
       return $para;
    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
