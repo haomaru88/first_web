92eb3a5559ec5be4479aba5de898239ee7a12442
haomaru88 <haomaru88@gmail.com>
2021-02-02 09:51:20 +0900

    Update select.php

--- application/views/select.php
+++ application/views/select.php
@@ -6,15 +6,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html class="no-js" lang="ko" dir="ltr">
 
 <head>
+   <style>
+      body {
+         background: linear-gradient(to left, #F5DF4D, #939597);
+         /* background: linear-gradient(to left, #3459a3, #3f98c1); */
+         display: table;
+         margin-left: auto;
+         margin-right: auto;
+         margin-top: 300px;
+      }
+
+      ul {
+         display: table;
+         margin-left: auto;
+         margin-right: auto;
+      }
+
+      h1 {
+         font-size: 64px;
+         text-shadow: 2px 2px 10px black;
+         /* text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black; */
+      }
+
+      .font_color {
+         color: white;
+         text-align: center;
+      }
+
+      .font_color2 {
+         color: black;
+         text-align: center;
+      }
+
+   </style>
 </head>
 
-<body>
-   <h2>지마텍 웹 모니터링 시스템</h2>
-
-   <ul>
-      <li><a href="/index.php/web_monitor/oxygen2019" title="2019 빈산소 자료">2019 빈산소 수괴 관측 자료</a></li>
-      <li><a href="/index.php/web_monitor/oxygen2020" title="2020 빈산소 자료">2020 빈산소  수괴 관측 자료</a></li>
-   </ul>  
-
+<body class="font_color">
+   <h1>지마텍 &nbsp모니터링 &nbsp시스템</h1>
+   <ul class="font_color2">
+      <li><a class="font_color2" href="/index.php/web_monitor/oxygen2019" title="2019 빈산소 자료">2019 빈산소 수괴 관측 자료</a></li>
+      <li><a class="font_color2" href="/index.php/web_monitor/oxygen2020" title="2020 빈산소 자료">2020 빈산소  수괴 관측 자료</a></li>
+   </ul>
 </body>
-
