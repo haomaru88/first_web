274837658bb5c816ea7eca6f16a36722a5ece4e3
haomaru88 <haomaru88@gmail.com>
2021-02-04 15:16:39 +0900

    Update home_body_first_page_2020.php

--- application/views/home_body_first_page_2020.php
+++ application/views/home_body_first_page_2020.php
@@ -201,13 +201,14 @@ function print_table_row($para)
       // 마커 이미지를 생성합니다    
       var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
       // var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption); 
-         
+      
+      var marker;
       for (var i = 0; i < positions.length; i ++) {
          // 마커를 생성합니다
-         var marker = new kakao.maps.Marker({
+         marker = new kakao.maps.Marker({
             map: map, // 마커를 표시할 지도
             position: positions[i].latlng, // 마커를 표시할 위치
-            title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            // title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
             clickable : false,
             image : markerImage // 마커 이미지 
          });
